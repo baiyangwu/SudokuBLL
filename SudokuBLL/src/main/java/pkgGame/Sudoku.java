7533863b0f9e0acf791fedffde9e7f4e23e8f85c
Baiyang Wu <Baiyang Wu@Wu-Laptop.fios-router.home>
2018-09-23 16:56:31 -0400

    .

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -6,11 +6,52 @@ import pkgHelper.LatinSquare;;
 
 public class Sudoku extends LatinSquare{
 
-	public Sudoku() {
+	public Sudoku(int iSize) throws java.lang.Exception{
 		super();
 	}
 	
-	public Sudoku(int[][] latinSquare) {
-		super(latinSquare);
+	public Sudoku(int[][] puzzle) throws java.lang.Exception{
+		super(puzzle);
 	}
+	
+	public int[][] getPuzzle()
+	{
+		return super.getLatinSquare();
+	}
+	
+	public int[] getRegion(int iCol, int iRow)
+	{
+		return null;
+	}	
+	public int[] getRegion(int r)
+	{
+		return null;
+	}
+	
+	public boolean isPartialSudoku()
+	{
+		return false;
+	}
+	public boolean isSudoku()
+	{
+		return false;
+	}
+
+	
+	public boolean isValidValue(int iCol, int iRow, int iValue)
+	{
+		return false;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
