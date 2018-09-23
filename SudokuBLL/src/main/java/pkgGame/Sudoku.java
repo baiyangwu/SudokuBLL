7f3f75de6266de5c08cd4eaf48b3e449b0ab941b
Baiyang Wu <Baiyang Wu@Wu-Laptop.fios-router.home>
2018-09-23 18:54:50 -0400

    p

--- SudokuBLL/src/main/java/pkgGame/Sudoku.java
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -5,14 +5,22 @@ import java.util.Arrays;
 import pkgHelper.LatinSquare;;
 
 public class Sudoku extends LatinSquare{
+	
+	private int iSize;
+	private int iSqrtSize;
 
 	public Sudoku(int iSize) throws java.lang.Exception{
-		super();
+		this.iSize = iSize;
+		if (iSize % Math.sqrt(iSize)==0) {
+			this.iSize = iSqrtSize;
+		}	
 	}
 	
 	public Sudoku(int[][] puzzle) throws java.lang.Exception{
 		super(puzzle);
 	}
+	
+	
 	
 	public int[][] getPuzzle()
 	{
@@ -21,12 +29,27 @@ public class Sudoku extends LatinSquare{
 	
 	public int[] getRegion(int iCol, int iRow)
 	{
+		if (0 <= iCol && iCol<=2) {
+			if (0<= iRow && iRow<=2) {
+				return Sudoku[0][0];
+			}
+		}
 		return null;
 	}	
+	
+	
 	public int[] getRegion(int r)
 	{
+		int [] reg = new int[super.getLatinSquare().length];
+		
+		int i = (r%iSqrtSize)*iSqrtSize;
+		int j = (r/iSqrtSize)*iSqrtSize;
+		int iMax = i + iSqrtSize;
+		int jMax = j + iSqrtSize;
+		int iCnt = ;
 		return null;
 	}
+	
 	
 	public boolean isPartialSudoku()
 	{
